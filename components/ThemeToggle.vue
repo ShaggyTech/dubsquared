0fb837ad9ac125218abc2fc2142e155e0671f8c5
ShaggyTech <beedat1@gmail.com>
2022-05-15 14:57:29 -0500

    fix: component ThemeToggle select box style refactor

--- components/ThemeToggle.vue
+++ components/ThemeToggle.vue
@@ -48,7 +48,7 @@
     <select
       v-if="currentStyle === 'select-box'"
       v-model="themeSetting"
-      class="w-full px-2 pr-3 py-1 outline-none rounded border bg-transparent text-gray-700 dark:text-gray-300 border-gray-900/10 dark:border-gray-50/[0.2]"
+      class="select-box w-full px-2 pr-3 py-1 rounded dark:bg-zinc-700"
     >
       <option
         v-for="theme in availableThemes"
