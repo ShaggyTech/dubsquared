9903ce3caa0b54d2e8c482f60768d65c6bc03259
ShaggyTech <beedat1@gmail.com>
2022-05-17 16:11:15 -0500

    refactor(component): 'ThemeToggle' color changes to match theme, add transition

--- components/ThemeToggle.vue
+++ components/ThemeToggle.vue
@@ -20,30 +20,46 @@
           <IconUil:moon style="font-size: 1.2em" />
         </span>
       </ListboxButton>
-      <ListboxOptions
-        class="p-1 absolute z-50 top-full right-0 outline-none bg-white rounded-lg ring-1 ring-gray-900/10 shadow-lg overflow-hidden w-36 py-1 text-sm text-gray-700 font-semibold dark:bg-gray-800 dark:ring-0 dark:highlight-white/5 dark:text-gray-300"
+      <transition
+        enter-active-class="transition duration-100 ease-out"
+        enter-from-class="transform scale-80 opacity-0"
+        enter-to-class="transform scale-100 opacity-100"
+        leave-active-class="transition duration-100 ease-out"
+        leave-from-class="transform scale-100 opacity-100"
+        leave-to-class="transform scale-80 opacity-0"
       >
-        <ListboxOption
-          v-for="theme in availableThemes"
-          :key="theme.key"
-          :value="theme.key"
-          :class="{
-            'py-2 px-2 flex items-center cursor-pointer': true,
-            'text-sky-500 bg-gray-100 dark:bg-gray-600/30':
-              themeSetting === theme.key,
-            'hover:bg-gray-50 dark:hover:bg-gray-700/30':
-              themeSetting !== theme.key,
-          }"
+        <ListboxOptions
+          :class="`
+          absolute top-full right-0 outline-none overflow-hidden z-50
+          w-36 p-1 py-1 rounded-lg shadow-lg
+          text-sm font-semibold text-gray-700 dark:text-gray-300 dark:highlight-white/5
+          bg-warm-gray-100 dark:bg-warm-gray-800
+          ring-1 ring-gray-900/10 dark:ring-0
+        `"
         >
-          <span class="text-sm mr-2">
-            <IconUil:sun v-if="theme.key === 'light'" />
-            <IconUil:moon v-else-if="theme.key === 'dark'" />
-            <IconUil:laptop v-else-if="theme.key === 'system'" />
-            <IconUil:clock v-else-if="theme.key === 'realtime'" />
-          </span>
-          {{ theme.text }}
-        </ListboxOption>
-      </ListboxOptions>
+          <ListboxOption
+            v-for="theme in availableThemes"
+            :key="theme.key"
+            :value="theme.key"
+            :class="{
+              'py-2 px-2 flex items-center cursor-pointer': true,
+              'text-warm-gray-400 bg-warm-gray-100 dark:bg-warm-gray-600/30':
+                themeSetting === theme.key,
+              'hover:bg-gray-50 dark:hover:bg-warm-gray-700/30':
+                themeSetting !== theme.key,
+            }"
+          >
+            <span class="text-sm mr-2">
+              <IconUil:moon v-if="theme.key === 'dark'" />
+              <IconUil:laptop v-else-if="theme.key === 'system'" />
+              <IconUil:clock v-else-if="theme.key === 'realtime'" />
+              <!-- default to 'light' -->
+              <IconUil:sun v-else />
+            </span>
+            {{ theme.text }}
+          </ListboxOption>
+        </ListboxOptions>
+      </transition>
     </Listbox>
     <select
       v-if="currentStyle === 'select-box'"
