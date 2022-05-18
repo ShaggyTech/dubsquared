c0e73aa0365ab6d5d234f35bc01e3c99986458eb
ShaggyTech <beedat1@gmail.com>
2022-05-18 01:32:16 -0500

    feat(mobile): first try at using safe-area-inset to handle phone notches

--- app.vue
+++ app.vue
@@ -1,8 +1,14 @@
 <template>
-  <Html :class="`${theme === 'dark' ? 'dark' : ''}`" :lang="locale">
+  <Html
+    :class="`${theme === 'dark' ? 'dark' : ''} ${'apple-safe-area'}`"
+    :lang="locale"
+  >
     <Head>
       <Title>Dubsquared - VW & Audi Specialists - Arlington, TX</Title>
-      <Meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Meta
+        name="viewport"
+        content="width=device-width, initial-scale=1 viewport-fit=cover"
+      />
       <Meta
         name="description"
         content="Dubsquared - Volkswagen & Audi Shop located in Arlington, TX"
@@ -10,7 +16,7 @@
       <Link rel="icon" type="image/png" href="/favicon.png" />
     </Head>
     <Body
-      class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900"
+      class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-gray-200 dark:bg-zinc-800"
     >
       <NuxtLayout>
         <NuxtPage />
@@ -26,3 +32,12 @@ AppSetup()
 const theme = useState<ITheme>('theme.current')
 const locale = useState<string>('locale.setting')
 </script>
+
+<style lang="scss">
+.apple-safe-area {
+  @supports (padding: max(0px)) {
+    padding-left: max(0vmin, env(safe-area-inset-left));
+    padding-right: max(0vmin, env(safe-area-inset-right));
+  }
+}
+</style>
