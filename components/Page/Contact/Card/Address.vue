c9f010fbbf66c8414c81efc3c20f6846806ebe01
ShaggyTech <beedat1@gmail.com>
2022-06-15 18:15:27 -0500

    feat(components): make reusable Card components for business Address and Hours

---
+++ components/Page/Contact/Card/Address.vue
@@ -0,0 +1,111 @@
+<template>
+  <div
+    v-motion
+    :initial="{
+      opacity: 0,
+    }"
+    :visible="{
+      opacity: 1,
+      backgroundImage: `url('/images/shop-frontage-SQ5.webp')`,
+      transition: {
+        duration: 500,
+        delay: 200,
+      },
+    }"
+    class="grid content-center bg-no-repeat bg-cover bg-center py-4em text-center text-xl rounded lg:(text-2xl tracking-wide)"
+  >
+    <a
+      v-motion
+      :initial="{
+        y: 20,
+        opacity: 0,
+      }"
+      :visible="{
+        y: 0,
+        opacity: 1,
+        transition: {
+          duration: 500,
+          delay: 500,
+        },
+      }"
+      href="https://www.google.com/maps/dir//Dubsquared,+1223+Colorado+Ln,+Arlington,+TX+76015/@32.7024485,-97.1279981,17z"
+      title="Google Maps"
+      target="_blank"
+      class="grid gap-y-8 p-2em mx-auto bg-zinc-900/90 backdrop-filter backdrop-blur-sm rounded"
+    >
+      <address class="grid gap-y-8 place-items-center">
+        <IconMdi:mapMarkerRadius
+          class="h-[2em] w-[2em] text-yellow-500 animate-bounce animate-duration-2000"
+        />
+        <span
+          v-motion
+          :initial="{
+            y: 20,
+            opacity: 0,
+          }"
+          :visible="{
+            y: 0,
+            opacity: 1,
+            transition: {
+              duration: 500,
+              delay: 600,
+            },
+          }"
+          class="flex flex-col font-bold text-stone-100 text-shadow-xl"
+        >
+          <span
+            v-motion
+            :initial="{
+              x: -50,
+              opacity: 0,
+            }"
+            :visible="{
+              x: 0,
+              opacity: 1,
+              transition: {
+                duration: 500,
+                delay: 600,
+              },
+            }"
+            >1223 Colorado Ln</span
+          >
+          <span
+            v-motion
+            :initial="{
+              x: -50,
+              opacity: 0,
+            }"
+            :visible="{
+              x: 0,
+              opacity: 1,
+              transition: {
+                duration: 500,
+                delay: 600,
+              },
+            }"
+            >Suite 105</span
+          >
+          <span
+            v-motion
+            :initial="{
+              x: -50,
+              opacity: 0,
+            }"
+            :visible="{
+              x: 0,
+              opacity: 1,
+              transition: {
+                duration: 600,
+                delay: 600,
+              },
+            }"
+            >Arlington, TX 76015</span
+          >
+        </span>
+      </address>
+      <div class="text-yellow-400 underline underline-offset-4">
+        Click for Google Maps Directions
+      </div>
+    </a>
+  </div>
+</template>
---
+++ components/Page/Contact/Card/Hours.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+const isBusinessOpen = ref<boolean>(true)
+
+onMounted(() => {
+  const date = new Date()
+  // Set business timezone
+  const timezone = -6
+  const timezoneOffset = (date.getTimezoneOffset() + timezone * 60) * 60 * 1000
+  // set date to account for actual business timezone
+  date.setTime(date.getTime() + timezoneOffset)
+  const day = date.getDay()
+  const hour = date.getHours()
+
+  // open M-F 9am-6pm, closed saturday and sunday
+  if (day < 1 || day > 5 || hour < 9 || hour > 18) isBusinessOpen.value = false
+  else isBusinessOpen.value = true
+})
+</script>
+
+<template>
+  <div
+    v-motion
+    :initial="{
+      y: 50,
+      opacity: 0,
+    }"
+    :visible="{
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 500,
+        delay: 500,
+      },
+    }"
+    class="grid gap-6 sm:gap-8 lg:gap-10 place-items-center text-center text-lg md:text-xl rounded lg:(text-2xl tracking-wide) h-full w-full py-[2em] bg-zinc-900 text-shadow-xl"
+  >
+    <IconIon:clock
+      :class="[
+        'h-[2.5em] w-[2.5em] animate-spin animate-duration-60s',
+        isBusinessOpen ? 'text-green-500' : 'text-red-500',
+      ]"
+    />
+    <div
+      v-motion
+      :initial="{
+        x: 50,
+        opacity: 0,
+      }"
+      :visible="{
+        x: 0,
+        opacity: 1,
+        transition: {
+          duration: 500,
+          delay: 500,
+        },
+      }"
+      class="text-1.5em"
+    >
+      Shop Hours
+      <hr
+        v-motion
+        :initial="{
+          x: 50,
+          opacity: 0,
+        }"
+        :visible="{
+          x: 0,
+          opacity: 1,
+          transition: {
+            duration: 500,
+            delay: 500,
+          },
+        }"
+        class="hr w-10ch mx-auto mt-3"
+      />
+    </div>
+
+    <div
+      v-motion
+      :initial="{
+        x: 50,
+        opacity: 0,
+      }"
+      :visible="{
+        x: 0,
+        opacity: 1,
+        transition: {
+          duration: 500,
+          delay: 500,
+        },
+      }"
+      class="grid gap-[0.5em]"
+    >
+      <span class="italic">Monday - Friday</span>
+      <span class="font-bold text-green-500"> 9am - 6pm </span>
+    </div>
+    <hr class="hr w-8ch mx-auto" />
+    <div
+      v-motion
+      :initial="{
+        x: 50,
+        opacity: 0,
+      }"
+      :visible="{
+        x: 0,
+        opacity: 1,
+        transition: {
+          duration: 500,
+          delay: 500,
+        },
+      }"
+      class="grid gap-[0.5em]"
+    >
+      <span class="italic">Saturday & Sunday</span>
+      <span class="font-bold text-red-500">CLOSED</span>
+    </div>
+  </div>
+</template>
