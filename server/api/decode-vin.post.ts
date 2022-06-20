b337bd1bf1d707625c6226faa28fd0a226b580f4
ShaggyTech <beedat1@gmail.com>
2022-06-20 17:52:03 -0500

    fix(api): route decode-vin.post remove not needed  options

--- server/api/decode-vin.post.ts
+++ server/api/decode-vin.post.ts
@@ -1,13 +1,7 @@
 export default defineEventHandler(async (event) => {
   const { vin }: { vin: string } = await useBody(event)
   const { Results }: any = await $fetch(
-    `https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/${vin}?format=json`,
-    {
-      method: 'get',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
+    `https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/${vin}?format=json`
   ).catch((error) => {
     return { error: 'Oops... Something went wrong ' + error }
   })
