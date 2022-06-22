a84e1ecf5bf1c4e10d3887640a064494a2813feb
ShaggyTech <beedat1@gmail.com>
2022-06-21 23:51:59 -0500

    refactor(fonts): load fonts via head link rels, no longer using css import() to do this

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
+<script lang="ts" setup>
+import '@formkit/themes/genesis'
+
+import { AppSetup } from './utils/app'
+import { ITheme } from './utils/theme'
+
+AppSetup()
+
+const theme = useState<ITheme>('theme.current')
+const locale = useState<string>('locale.setting')
+</script>
+
 <template>
   <Html :class="`${theme === 'dark' ? 'dark' : ''}`" :lang="locale">
     <Head>
@@ -11,6 +23,13 @@
         content="Dubsquared - Volkswagen & Audi Shop located in Arlington, TX"
       />
       <Link rel="icon" type="image/png" href="/favicon.png" />
+      <Link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Teko:wght@300;400;500;600;700&display=swap"
+        defer="true"
+      />
+      <Link rel="preconnect" href="https://fonts.gstatic.com" />
+      <Link rel="preconnect" href="https://fonts.googleapis.com" />
     </Head>
     <Body
       class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-gray-200 dark:bg-zinc-800"
@@ -21,15 +40,6 @@
     </Body>
   </Html>
 </template>
-
-<script lang="ts" setup>
-import '@formkit/themes/genesis'
-import { AppSetup } from './utils/app'
-import { ITheme } from './utils/theme'
-AppSetup()
-const theme = useState<ITheme>('theme.current')
-const locale = useState<string>('locale.setting')
-</script>
 
 <style lang="scss">
 // padding for phone screen 'notches'
