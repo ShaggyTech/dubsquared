73ece45132e0ae0d5b20241331befb7bad8beb20
ShaggyTech <beedat1@gmail.com>
2022-06-23 13:02:35 -0500

    performance(PageLayoutFooter): google maps embed, use object instead of ifram, lazy load only on seen

--- components/Page/Layout/Footer.vue
+++ components/Page/Layout/Footer.vue
@@ -1,17 +1,62 @@
+<script lang="ts" setup>
+import { IApp } from '~/utils/app'
+
+const app = useState<IApp>('app')
+
+const observer = useState<IntersectionObserver | undefined>(
+  'PageLayoutFooter-observer'
+)
+const container = ref<HTMLElement>()
+const seen = ref<boolean>(false)
+
+const objectDataUrl = ref(
+  'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d107441.24127096374!2d-97.19730196382534!3d32.6983079!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e7d2df9e608cb%3A0x7cd9aab902cff64b!2sDubsquared!5e0!3m2!1sen!2sus!4v1645761293912!5m2!1sen!2sus'
+)
+const objectData = computed<string>(() =>
+  seen.value ? objectDataUrl.value : ''
+)
+
+onMounted(async () => {
+  await nextTick()
+  observer.value = new IntersectionObserver((entries) => {
+    entries.forEach(({ isIntersecting }) => {
+      if (isIntersecting) {
+        seen.value = true
+        observer?.value?.disconnect()
+      }
+    })
+  })
+  if (container.value && observer.value) observer.value.observe(container.value)
+})
+
+onUnmounted(() => {
+  observer.value?.disconnect()
+  observer.value = undefined
+})
+</script>
+
+<script lang="ts">
+export default { name: 'PageLayoutFooter' }
+</script>
+
 <template>
   <footer
+    ref="container"
     class="relative w-full h-fit border-t lg:border-gray-900/10 dark:border-gray-50/[0.2]"
   >
     <section
       class="flex flex-col h-fit w-full max-w-8xl mx-auto apple-safe-area"
     >
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d107441.24127096374!2d-97.19730196382534!3d32.6983079!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e7d2df9e608cb%3A0x7cd9aab902cff64b!2sDubsquared!5e0!3m2!1sen!2sus!4v1645761293912!5m2!1sen!2sus"
+      <object
+        :data="objectData"
+        height="800"
+        width="800"
         class="border-none h-64 w-full mx-auto py-8 px-4 lg:px-8"
         allowfullscreen
         loading="lazy"
         title="Google Maps Dubsquared"
-      ></iframe>
+        type="text/html"
+      ></object>
       <div class="w-full py-4 px-4 lg:px-8 text-center md:text-left">
         <div class="mb-1">
           {{ app.name }}
@@ -25,8 +70,3 @@
     </section>
   </footer>
 </template>
-
-<script lang="ts" setup>
-import { IApp } from '~/utils/app'
-const app = useState<IApp>('app')
-</script>
