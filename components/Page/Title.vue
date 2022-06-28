90c4af3d78eb7cf2b7221a3cfb155b52a3a8f8f3
ShaggyTech <beedat1@gmail.com>
2022-06-27 23:18:14 -0500

    refactor(PageTitle): use h1 instead of div

--- components/Page/Title.vue
+++ components/Page/Title.vue
@@ -1,9 +1,9 @@
 <template>
-  <div
+  <h1
     class="text-4x md:text-5xl lg:text-6xl tracking-wider font-kanit font-bold"
   >
     <slot>{{ text }}</slot>
-  </div>
+  </h1>
 </template>
 
 <script lang="ts" setup>
