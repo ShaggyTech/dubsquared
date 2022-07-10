c676a541d2e52ca03d732c9846fc7c06ca02e3e3
ShaggyTech <beedat1@gmail.com>
2022-07-10 17:00:30 -0500

    feat(components): new testimonial components, Card and Stars, new SVGStar component

---
+++ components/Testimonial/Card.vue
@@ -0,0 +1,154 @@
+<script setup lang="ts">
+import { useMotion } from '@vueuse/motion'
+import type { MotionVariants } from '@vueuse/motion'
+
+interface Props {
+  by?: string
+  date?: string
+  paragraph?: string
+  numStars?: 0 | 1 | 2 | 3 | 4 | 5
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = withDefaults(defineProps<Props>(), {
+  by: '',
+  date: '',
+  paragraph: '',
+  numStars: 5,
+})
+
+const cardRef = ref<HTMLElement>()
+const cardMotionVariants = ref<MotionVariants>({
+  initial: {
+    opacity: 0,
+    scale: 0.98,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 500,
+      delay: 100,
+    },
+  },
+})
+
+const nameRef = ref<HTMLElement>()
+const nameMotionVariants = ref<MotionVariants>({
+  initial: {
+    opacity: 0,
+    scale: 0.98,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 500,
+      delay: 300,
+    },
+  },
+})
+
+const dividerRef = ref<HTMLElement>()
+const dividerMotionVariants = ref<MotionVariants>({
+  initial: {
+    x: -75,
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 500,
+      delay: 500,
+    },
+  },
+})
+
+const iconRef = ref<HTMLElement>()
+const iconMotionVariants = ref<MotionVariants>({
+  initial: {
+    y: -50,
+    opacity: 0,
+    scale: 0.8,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 400,
+      delay: 400,
+    },
+  },
+})
+
+const paragraphRef = ref<HTMLElement>()
+const paragraphMotionVariants = ref<MotionVariants>({
+  initial: {
+    y: -40,
+    opacity: 0,
+    scale: 0.85,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 400,
+      delay: 300,
+    },
+  },
+})
+
+onMounted(() => {
+  useMotion(cardRef, cardMotionVariants)
+  useMotion(nameRef, nameMotionVariants)
+  useMotion(dividerRef, dividerMotionVariants)
+  useMotion(iconRef, iconMotionVariants)
+  useMotion(paragraphRef, paragraphMotionVariants)
+})
+</script>
+
+<script lang="ts">
+export default { name: 'TestimonialCard' }
+</script>
+
+<template>
+  <div
+    ref="cardRef"
+    :class="`
+      w-fit p-10 rounded-lg shadow-lg bg-neutral-200/50 border border-red-900/40
+      dark:(bg-zinc-800/90 border-red-900/20)
+    `"
+  >
+    <div class="flex flex-col gap-4 text-left">
+      <p ref="nameRef" class="flex items-baseline gap-2 w-full">
+        <span class="text-stone-600 text-xl font-bold dark:text-stone-200">
+          {{ by }}
+        </span>
+        <span class="ml-1 text-stone-500 text-sm dark:text-stone-300">
+          {{ date }}
+        </span>
+      </p>
+      <div ref="iconRef" class="flex items-center">
+        <SVGStar
+          v-for="i in numStars"
+          :key="i"
+          :filled="true"
+          class="w-4 h-4"
+        />
+        <SVGStar v-for="i in 5 - numStars" :key="i" class="h-4 w-4" />
+      </div>
+      <hr
+        ref="dividerRef"
+        class="mt-2 border-t-2 border-t-red-900 dark:border-t-red-900/50"
+      />
+      <p
+        ref="paragraphRef"
+        class="mt-4 max-w-md sm:max-w-xs text-lg dark:text-white"
+      >
+        "{{ paragraph }}"
+      </p>
+    </div>
+  </div>
+</template>
---
+++ components/Testimonial/Stars.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface Props {
+  starClass?: string
+  numStars?: 0 | 1 | 2 | 3 | 4 | 5
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = withDefaults(defineProps<Props>(), {
+  starClass: 'h-4 w-4',
+  numStars: 5,
+})
+</script>
+
+<script lang="ts">
+export default { name: 'TestimonialStars' }
+</script>
+
+<template>
+  <div class="flex items-center mt-1">
+    <SVGStar
+      v-for="i in numStars"
+      :key="i"
+      :class="`${starClass}`"
+      :filled="true"
+    />
+  </div>
+</template>
