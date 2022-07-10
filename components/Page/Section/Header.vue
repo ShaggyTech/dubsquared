aa8dc1503affcbe9d9057c1fefdac115981b4646
ShaggyTech <beedat1@gmail.com>
2022-07-10 16:57:15 -0500

    feat(components): new PageSectionHeader

---
+++ components/Page/Section/Header.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+interface Props {
+  title?: string
+  paragraph?: string
+  anchorText: string
+  anchorTo?: string
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = withDefaults(defineProps<Props>(), {
+  title: '',
+  paragraph: '',
+  anchorText: '',
+  anchorTo: undefined,
+})
+</script>
+
+<script lang="ts">
+export default { name: 'PageSectionHeader' }
+</script>
+
+<template>
+  <div class="grid place-items-center">
+    <div
+      class="flex flex-col items-center text-center prose lg:prose-lg xl:prose-xl"
+    >
+      <slot name="icon" />
+      <h1 class="!mt-8 font-nunito-sans font-bold tracking-wider">
+        {{ title }}
+      </h1>
+      <hr
+        class="w-10/12 md:w-full !mt-0 !mb-12 !border-t-2 !border-t-red-900 dark:!border-t-red-900"
+      />
+      <p class="text-2xl font-semibold">
+        {{ paragraph }}
+      </p>
+      <Anchor v-if="anchorTo" :to="anchorTo" class="!mt-8 !text-2xl !font-bold">
+        {{ anchorText }}
+      </Anchor>
+    </div>
+  </div>
+</template>
