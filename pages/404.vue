2cfa1ea0cb13b2ab79a5f6dae93ee8b224dfa897
ShaggyTech <beedat1@gmail.com>
2022-07-17 20:58:34 -0500

    refactor(404): move error.vue to 404.vue

--- pages/404.vue
+++ pages/404.vue
@@ -12,8 +12,15 @@ const props = withDefaults(defineProps<Props>(), {
 
 definePageMeta({
   layout: 'page',
-  title: `Error ${props.error?.statusCode}`,
-  description: `An error occured: ${props.error?.statusMessage}`,
+})
+useHead({
+  title: `Error ${props.error?.statusCode ? props.error.statusCode : ''}`,
+  meta: [
+    {
+      name: 'description',
+      content: `An error occured: ${props.error?.statusMessage}`,
+    },
+  ],
 })
 
 const handleError = () => {
@@ -27,16 +34,18 @@ const handleError = () => {
   >
     <div class="flex flex-col items-center gap-6">
       <h1
-        class="title bg-gradient text-gradient !mb-0 !text-7xl font-semibold md:!text-9xl"
+        class="title bg-gradient text-gradient !mb-0 !text-5xl font-semibold md:!text-9xl"
       >
         Oops! An error has occured.
       </h1>
-      <h2 class="mb-12 text-lg text-gray-200 md:text-xl">
+      <h2 v-if="error?.message" class="mb-12 text-3xl text-gray-200 md:text-xl">
         Error Message: {{ error?.message }}
       </h2>
       <button type="button" class="flex space-x-2" @click="handleError">
-        <IconMdi:arrowLeft class="h-6 w-6" />
-        <span>Home page</span>
+        <IconMdi:arrowLeft
+          class="text-3xl text-yellow-500 dark:text-yellow-500"
+        />
+        <span class="text-3xl">Back to Home page</span>
       </button>
     </div>
   </div>
