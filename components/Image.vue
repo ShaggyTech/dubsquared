dd892c49ceef380b2671438c22d5f99687cd6781
ShaggyTech <beedat1@gmail.com>
2022-07-24 22:06:06 -0500

    feat(components): new component 'Image'

---
+++ components/Image.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = defineProps<{
+  alt: string
+  src: string
+  lazy?: boolean | 'false' | 'true'
+}>()
+</script>
+
+<script lang="ts">
+export default {
+  name: 'Image',
+  inheritAttrs: false,
+}
+</script>
+
+<template>
+  <figure>
+    <img
+      v-bind="$attrs"
+      :alt="alt"
+      :src="src"
+      :loading="Boolean(lazy) ? 'lazy' : 'eager'"
+    />
+  </figure>
+</template>
