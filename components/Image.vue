f9a60539fdc2f3f99fcda309e817e621ac2f849e
ShaggyTech <beedat1@gmail.com>
2022-07-25 00:56:49 -0500

    refactor(Image): add optional lazy loading with placeholder image using observer

--- components/Image.vue
+++ components/Image.vue
@@ -1,10 +1,65 @@
 <script lang="ts" setup>
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const props = defineProps<{
-  alt: string
+interface Props {
   src: string
+  placeholderSrc?: string
+  alt?: string
   lazy?: boolean | 'false' | 'true'
-}>()
+  observerKey?: string
+  seen?: boolean | 'false' | 'true'
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = withDefaults(defineProps<Props>(), {
+  src: undefined,
+  placeholderSrc: undefined,
+  alt: undefined,
+  lazy: false,
+  observerKey: undefined,
+  seen: undefined,
+})
+
+const containerRef = ref<HTMLElement>()
+
+// optional internal observer state, if props.observerKey
+const observer = props.observerKey
+  ? useState<IntersectionObserver | undefined>(props.observerKey)
+  : undefined
+const container = props.observerKey
+  ? useState(`${props.observerKey}-container`, () => containerRef)
+  : undefined
+const seen = props.observerKey
+  ? useState(`${props.observerKey}-seen`, () => false)
+  : undefined
+
+// image state
+const imgSrc = computed(() => {
+  if (props.seen !== undefined || props.observerKey) {
+    if (props.seen || seen?.value) {
+      return props.src
+    } else {
+      return props.placeholderSrc
+    }
+  } else return props.src
+})
+
+onMounted(async () => {
+  await nextTick()
+  // only if lazy and props.observerKey provided
+  if (props.lazy && observer && container?.value) {
+    observer.value = new IntersectionObserver((entries) => {
+      const el = entries[0]
+      if (el.isIntersecting) {
+        if (seen) seen.value = true
+        observer.value?.disconnect()
+      }
+    })
+
+    if (observer.value) observer.value.observe(container.value)
+  }
+})
+
+onUnmounted(() => {
+  observer?.value?.disconnect()
+})
 </script>
 
 <script lang="ts">
@@ -15,11 +70,11 @@ export default {
 </script>
 
 <template>
-  <figure>
+  <figure ref="containerRef">
     <img
       v-bind="$attrs"
       :alt="alt"
-      :src="src"
+      :src="imgSrc"
       :loading="Boolean(lazy) ? 'lazy' : 'eager'"
     />
   </figure>
