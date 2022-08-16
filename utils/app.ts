276bd0fe17e0fb3d5a34e1c35015a5fd0fd690be
ShaggyTech <beedat1@gmail.com>
2022-08-15 21:41:23 -0500

    refactor(app): move appName and currentTheme from app.vue to utils/app

--- utils/app.ts
+++ utils/app.ts
@@ -15,6 +15,15 @@ export function AppSetup() {
   const pinia = getActivePinia()
 
   // initialize stores
-  useAppStore(pinia)
-  useThemeStore(pinia)
+  const appStore = useAppStore(pinia)
+  const themeStore = useThemeStore(pinia)
+
+  // minimum store state we need to expose for usage in app.vue
+  const { appName } = storeToRefs(appStore)
+  const { currentTheme } = storeToRefs(themeStore)
+
+  return {
+    appName,
+    currentTheme,
+  }
 }
