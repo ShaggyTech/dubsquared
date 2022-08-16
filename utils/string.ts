f8cf1b2d609e516df2fdc1bc86c68cac4dd81613
ShaggyTech <beedat1@gmail.com>
2022-08-15 21:42:19 -0500

    chore(comments): add doc comments to utils/string titleCase function

--- utils/string.ts
+++ utils/string.ts
@@ -1,3 +1,10 @@
+/**
+ * Formats a string
+ * - Splits string at space characters to get words.
+ * - Capitalizes first char of each word and lowercases remainder of word
+ * @param string string to format
+ * @returns formatted string
+ */
 export const titleCase = (string: string): string => {
   return string
     .split(' ')
