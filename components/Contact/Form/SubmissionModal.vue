bf7a491480438a08e46807006a9d3e7eeee3de8f
ShaggyTech <beedat1@gmail.com>
2022-08-17 22:43:53 -0500

    feat(components): new components ContactFormSubmissionMessage and ContactFormSubmissionModal

---
+++ components/Contact/Form/SubmissionModal.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+  show: boolean
+}
+defineProps<Props>()
+</script>
+
+<script lang="ts">
+export default {
+  name: 'ContactFormSubmissionModal',
+}
+</script>
+
+<template>
+  <Modal :show="show">
+    <ContactFormSubmissionMessage />
+  </Modal>
+</template>
