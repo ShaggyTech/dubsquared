55ec63f9850782d20ebd4163d987b363f9963555
ShaggyTech <beedat1@gmail.com>
2022-09-03 19:54:12 -0500

    fix: fix path in deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -14,6 +14,7 @@ wait $pid
 echo "New build completed, running pm2 reload"
 # I'm using nvm on my production host, so I have to specify pm2 binary path
 # to find your path, run 'pm2 startup' and use the path from the printed string with pm2 in it
-/home/shaggy/.nvm/versions/node/v16.17.0/lib/node_modules/pm2/bin/pm2 reload ../ecosystem.config.js --update-env
+cd ..
+/home/shaggy/.nvm/versions/node/v16.17.0/lib/node_modules/pm2/bin/pm2 reload ecosystem.config.js --update-env
 
 echo "pm2 reloaded"
