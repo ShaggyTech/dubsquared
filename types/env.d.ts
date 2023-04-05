323641349ca8840b09c128240fa51690c5c84bec
ShaggyTech <beedat1@gmail.com>
2023-04-05 15:44:51 -0500

    revert(types): remove GlobalSecrets interface that was added for Cloudflare compatibility

--- types/env.d.ts
+++ types/env.d.ts
@@ -12,20 +12,6 @@ declare module '@vue/runtime-core' {
   }
 }
 
-// needed for Cloudflare wrangler env variables
-declare global {
-  interface GlobalSecrets {
-    NUXT_SENDGRID_API_KEY: string
-    NUXT_SENDGRID_FROM_EMAIL: string
-    NUXT_SENDGRID_FROM_NAME: string
-    NUXT_SENDGRID_TO_EMAIL: string
-    NUXT_SENDGRID_TO_NAME: string
-    NUXT_PUBLIC_COMPANY_EMAIL: string
-    NUXT_PUBLIC_COMPANY_PHONE: string
-  }
-  type GS = typeof globalThis & GlobalSecrets
-}
-
 declare module '#app' {
   interface PageMeta {
     title?: string
