5ab4b47f0d0e37e80a60e429fb95de046e90f4b1
ShaggyTech <beedat1@gmail.com>
2023-04-19 16:50:53 -0500

    feat(vehicles): use @shaggytools/nhtsa-api-wrapper to decode vehicle information

--- server/api/decode-vin.post.ts
+++ server/api/decode-vin.post.ts
@@ -1,13 +1,12 @@
-import type { DecodeVinValuesResults, DecodeVinValuesResponse } from '~/types'
+import { DecodeVinValues } from '@shaggytools/nhtsa-api-wrapper'
+
+import type { DecodeVinValuesResults } from '@shaggytools/nhtsa-api-wrapper'
 
 export default defineEventHandler(
   async (event): Promise<DecodeVinValuesResults | undefined> => {
     const { vin }: { vin: string } = await readBody(event)
 
-    const response: DecodeVinValuesResponse =
-      await $fetch<DecodeVinValuesResponse>(
-        `https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/${vin}?format=json`
-      ).catch((error) => error)
+    const response = await DecodeVinValues(vin)
 
     // if any relevant data is returned from nhtsa api, return the Results object
     if (response.Count) return response.Results[0]
